30f3998ab693bdc663fccc9940000750aae1a666
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2022-01-16 10:55:10 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,5 @@
 #include <iostream>
-#inlcude <ColorCodeDefines.h>
-#include <assert.h>
-
-void testNumberToPair(int pairNumber,
-    TelCoColorCoder::MajorColor expectedMajor,
-    TelCoColorCoder::MinorColor expectedMinor)
-{
-    TelCoColorCoder::ColorPair colorPair =
-        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-    std::cout << "Got pair " << colorPair.ToString() << std::endl;
-    assert(colorPair.getMajor() == expectedMajor);
-    assert(colorPair.getMinor() == expectedMinor);
-}
-
-void testPairToNumber(
-    TelCoColorCoder::MajorColor major,
-    TelCoColorCoder::MinorColor minor,
-    int expectedPairNumber)
-{
-    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
-    std::cout << "Got pair number " << pairNumber << std::endl;
-    assert(pairNumber == expectedPairNumber);
-}
+#inlcude <TestDefinitions.h>
 
 int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
