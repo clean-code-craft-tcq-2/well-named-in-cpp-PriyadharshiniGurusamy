2d872680c300201bdc1976f7ca16afc0c29d5e40
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2022-01-17 10:33:23 +0530

    Create ColorCodeInterface.h

---
+++ ColorCodeInterface.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <ColorCodeDefines.h> 
+#include <ColorPair.h>
+
+namespace TelCoColorCoder
+{
+    ColorPair GetColorFromPairNumber(int pairNumber)
+    {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) 
+    {
+        return major * numberOfMinorColors + minor + 1;
+    }    
+    void ReadManual()
+    {
+       std::cout<<"PairNumber  -  ColorPair"<<std::endl;
+       for(int PairNumber =1; PairNumber<=25;PairNumber++)
+       {
+          TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(PairNumber);
+          std::cout<<PairNumber<<"     -    "<<colorPair.ToString()<<std::endl;
+       }
+    }
+}
