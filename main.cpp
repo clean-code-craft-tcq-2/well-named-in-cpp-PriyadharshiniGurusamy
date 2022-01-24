9d41def183ee835b4d7d44fe6ed183a23e0cc687
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2022-01-24 10:18:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
     
-    TelCoColorCoder::ReadManual();
+    testPrintColorPairManual();
 
     return 0;
 }
