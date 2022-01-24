4d55d182a07703ca846da5a173447f362755e2aa
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2022-01-24 10:18:22 +0530

    Update TestDefinitions.h

--- TestDefinitions.h
+++ TestDefinitions.h
@@ -2,6 +2,9 @@
 #include "ColorCodeInterface.h"
 #include <assert.h>
 
+int printCounter = 0;
+int getColorPairCounter = 0;
+
 void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
     TelCoColorCoder::MinorColor expectedMinor)
@@ -21,4 +24,22 @@ void testPairToNumber(
     int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
+}
+
+void mockPrint(int /*PairNumber*/, std::string /*ColorPair*/)
+{
+    printCounter++;
+}
+TelCoColorCoder::ColorPair mockGetColorPair(int PairNumber)
+{
+    getColorPairCounter++;
+    return TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+}
+
+void testPrintColorPairManual()
+{
+    printCounter = 0;
+    getColorPairCounter = 0;  
+    TelCoColorCoder::PrintManual(mockGetColorPair, mockPrint);
+    assert(printCounter == 23);
 }
